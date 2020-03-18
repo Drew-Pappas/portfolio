65d63ecb1ae6d84898844939a04c8e18331a520b
Drew <drewpap@umich.edu>
2020-03-18 00:30:29 -0400

    major changes to formatting & color scheme

--- js/js.js
+++ js/js.js
@@ -11,7 +11,22 @@ function changeMenu() {
 function unhideMenu() {
     console.log("working")
     var navs = document.querySelector("nav");
-    if (navs.classList.contains("hidden") && window.innerWidth >= 800) {
+    if (navs.classList.contains("hidden") && window.innerWidth >= 750) {
         navs.classList.remove("hidden")
     }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
